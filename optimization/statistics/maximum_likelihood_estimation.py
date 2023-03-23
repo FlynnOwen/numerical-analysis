ca36fcee03f79b27d5d6c706af3321ef28197bb1
Flynn Owen <flynnoo49@gmail.com>
2023-03-23 20:29:41 +1300

    add binomial mle

--- optimization/statistics/maximum_likelihood_estimation.py
+++ optimization/statistics/maximum_likelihood_estimation.py
@@ -4,6 +4,7 @@ optimization function.
 """
 from typing import List
 from abc import ABC, abstractmethod
+import math
 
 from scipy.optimize import minimize
 import numpy as np
@@ -33,7 +34,6 @@ class MaximumLikelihoodEstimatorBase(ABC):
         fitted_model = minimize(
             self.mle_estimate,
             starting_value,
-            options={"disp": True},
             method="Nelder-Mead"
         )
 
@@ -46,7 +46,6 @@ class MaximumLikelihoodEstimatorBase(ABC):
         fitted_model = minimize(
             self.mle_regression,
             starting_value,
-            options={"disp": True},
             method="Nelder-Mead"
         )
 
@@ -69,7 +68,7 @@ class BernoulliMLE(MaximumLikelihoodEstimatorBase):
 
     def mle_estimate(self, coeffs: tuple):
         p = coeffs[0]
-        n = len(self.x_data)
+        n = len(self.y_data)
 
         # log-likelihood function of Bernoulli data
         return - ((np.log(p) * sum(self.y_data)) + (np.log(1 - p) * (n - sum(self.y_data))))
@@ -86,11 +85,66 @@ class BernoulliMLE(MaximumLikelihoodEstimatorBase):
         beta = self.regression_coeff[1]
         # Expit (transform) of observed data.
         return [1/(1 + np.exp(-(alpha + (i*beta)))) for i in x_observed]
+    
+
+class BinomialMLE(MaximumLikelihoodEstimatorBase):
+    # https://www.analyticsvidhya.com/blog/2022/02/decoding-logistic-regression-using-mle/
+    def __init__(self, x_data, y_data, n_trials):
+        self.x_data = x_data
+        self.y_data = y_data
+        self.n_trials = n_trials
+        self.model = None
+
+    def mle_estimate(self, coeffs: tuple):
+        p = coeffs[0]
+        n = len(self.y_data)
+
+        # log-likelihood function of Binomial data
+        return - (sum([np.log(math.comb(self.n_trials, i)) for i in self.y_data]) + np.log(p)*sum(self.y_data) + np.log(1 - p)*(n*self.n_trials - sum(self.y_data)))
+    
+    def mle_regression(self, coeffs: tuple):
+        alpha = coeffs[0]
+        beta = coeffs[1]
+
+        # log-likelihood function of Bernoulli distribution, with parameter substituted for inverse link transformation.
+        return - (sum([np.log(math.comb(self.n_trials, i)) for i in self.y_data]) + (sum([self.y_data[i] * (alpha + beta * self.x_data[i]) for i in range(len(self.x_data))]) - sum([np.log(1 + np.exp(alpha + beta * i)) for i in self.x_data])))
+
+    def predict(self, x_observed):
+        alpha = self.regression_coeff[0]
+        beta = self.regression_coeff[1]
+        # Expit (transform) of observed data.
+        return [1/(1 + np.exp(-(alpha + (i*beta)))) for i in x_observed]
+
+
+def bernoulli_fit():
+    mle = BernoulliMLE(x_data=[70, 80, 90, 70, 75, 80, 10, 11, 5, 15],
+                       y_data=[1, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+    mle.fit_estimate()
+    print(mle.estimate_model)
+
+    mle.fit_regression()
+    print(mle.regression_model)
+
+    print(mle.predict([10, 5, 8, 90]))
+
+
+def binomial_fit():
+    mle = BinomialMLE(x_data=[70, 80, 90, 70, 75, 80, 10, 11, 5, 15],
+                      y_data=[7, 8, 9, 5, 6, 4, 5, 6, 8, 9],
+                      n_trials=10)
+    mle.fit_estimate()
+    print(mle.estimate_model)
+
+    mle.fit_regression()
+    print(mle.regression_model)
+
+    print(mle.predict([10, 5, 8, 90]))
 
 
 if __name__ == '__main__':
-    mle = BernoulliMLE(x_data=[70, 80, 90, 70, 75, 80, 10, 11, 5, 15],
-                       y_data=[1, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+    mle = BinomialMLE(x_data=[70, 80, 90, 70, 75, 80, 10, 11, 5, 15],
+                      y_data=[7, 8, 9, 5, 6, 4, 5, 6, 8, 9],
+                      n_trials=10)
     mle.fit_estimate()
     print(mle.estimate_model)
 
