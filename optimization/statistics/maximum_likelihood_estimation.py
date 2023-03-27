10705b49188eb6f82cc63fdb49308fea65c0f143
Flynn Owen <flynnoo49@gmail.com>
2023-03-27 23:01:23 +1300

    add Gaussian MLE

--- optimization/statistics/maximum_likelihood_estimation.py
+++ optimization/statistics/maximum_likelihood_estimation.py
@@ -134,7 +134,7 @@ class PoissonlMLE(MaximumLikelihoodEstimatorBase):
         beta = coeffs[1]
 
         # log-likelihood function of Poisson distribution, with parameter substituted for inverse link transformation.
-        return - (-sum([np.exp(alpha + beta*i) for i in self.x_data]) - sum([np.log(math.factorial(i)) for i in self.y_data]) + sum([self.y_data[i] *(alpha + beta*self.x_data[i]) for i in range(len(self.x_data))]))
+        return - (-sum([np.exp(alpha + beta*i) for i in self.x_data]) - sum([np.log(math.factorial(i)) for i in self.y_data]) + sum([self.y_data[i]*(alpha + beta*self.x_data[i]) for i in range(len(self.x_data))]))
 
     def predict(self, x_observed):
         alpha = self.regression_coeff[0]
@@ -142,6 +142,49 @@ class PoissonlMLE(MaximumLikelihoodEstimatorBase):
 
         # Exponential (transform) of observed data.
         return [np.exp(alpha + (i*beta)) for i in x_observed]
+    
+
+class GaussianMLE(MaximumLikelihoodEstimatorBase):
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+        self.model = None
+
+    def mle_estimate(self, coeffs: tuple):
+        mu = coeffs[0]
+        sigma = coeffs[1]
+        n = len(self.y_data)
+
+        # log-likelihood function of Gaussian data
+        return - (-(n/2)* np.log(2*np.pi) - (n/2)*np.log(sigma**2) - (1/(2*sigma**2))*sum((self.y_data - mu)**2))
+    
+    def fit_estimate(self, starting_value: tuple[int] = (0.5, 0.5)):
+        """ Fits MLE given data.
+        """
+        fitted_model = minimize(
+            self.mle_estimate,
+            starting_value,
+            method="Nelder-Mead"
+        )
+
+        self.estimate_model = fitted_model
+        self.estimate_coeff = fitted_model.x
+    
+    def mle_regression(self, coeffs: tuple):
+        alpha = coeffs[0]
+        beta = coeffs[1]
+        sigma = self.estimate_coeff[1]
+        n = len(self.y_data)
+
+        # log-likelihood function of Poisson distribution, with parameter substituted for inverse link transformation.
+        return - (-(n/2)* np.log(2*np.pi) - (n/2)*np.log(sigma**2) - (1/(2*sigma**2))*sum([(self.y_data[i] - (alpha + self.x_data[i]*beta))**2 for i in range(n)]))
+
+    def predict(self, x_observed):
+        alpha = self.regression_coeff[0]
+        beta = self.regression_coeff[1]
+
+        # Exponential (transform) of observed data.
+        return [alpha + (i*beta) for i in x_observed]
 
 
 def bernoulli_fit():
@@ -169,9 +212,9 @@ def binomial_fit():
     print(mle.predict([10, 5, 8, 90]))
 
 
-if __name__ == '__main__':
-    mle = PoissonlMLE(x_data=[70, 80, 90, 50, 60, 40, 50, 60, 80, 90],
-                      y_data=[7, 8, 9, 5, 6, 4, 5, 6, 8, 9])
+def poisson_fit():
+    mle = PoissonlMLE(x_data=[70, 80, 90, 50, 60, 40, 50, 60, 80, 90, 10, 20, 30, 10],
+                      y_data=[7, 8, 9, 5, 6, 4, 5, 6, 8, 9, 1, 2, 3, 1])
     mle.fit_estimate()
     print(mle.estimate_model)
 
@@ -179,3 +222,19 @@ if __name__ == '__main__':
     print(mle.regression_model)
 
     print(mle.predict([10, 60, 40, 90]))
+
+
+def gaussian_fit():
+    mle = GaussianMLE(x_data=[70, 80, 90, 70, 75, 80, 10],
+                      y_data=[7, 8, 9, 7, 7.5, 8, 1])
+    mle.fit_estimate()
+    print(mle.estimate_model)
+      
+    mle.fit_regression()
+    print(mle.regression_model)
+
+    print(mle.predict([10, 5, 8, 90]))
+
+
+if __name__ == '__main__':
+    gaussian_fit()
