c5f3cbfd25f3f8d081b79704c143b30a2b25bf5d
Flynn Owen <flynnoo49@gmail.com>
2023-04-02 13:39:00 +1200

    add skeleton example of adversarial discriminator

---
+++ optimization/statistics/gaussian_adversarial_generator.py
@@ -0,0 +1,11 @@
+"""
+Playing around with the basics of 
+adversarial methods of data generation.
+
+This basic example is a univariate Gaussian distribution
+as the generator, with the probability distribution estimated
+using MLE. The 'adversary' is a basic logistic regression.
+
+Both are implemented from scratch using SciPy.
+"""
+
