e7f665fb93c53cee605713c4362d82d92048db33
Flynn Owen <flynnoo49@gmail.com>
2024-01-03 22:21:56 +1300

    decouple data and functionality

--- optimization/statistics/regression/nonlinear_regression.py
+++ optimization/statistics/regression/nonlinear_regression.py
@@ -8,60 +8,96 @@ Nonlinear regression in the form
  - Piecewise continuous linear
  - Piecewise linear basis
 """
-from typing import List
-from itertools import batched
-from dataclasses import dataclass
-from functools import total_ordering
+from __future__ import annotations
 
-from optimization.statistics.regression._linear_regression import LinearRegression
+from dataclasses import dataclass
+from typing import Any
+import numpy as np
+
+from optimization.statistics.regression.linear_regression import LinearRegression
 
 
 @dataclass
-@total_ordering
-class Segment:
-    def __init__(self, x_data: list[float], y_data: list[float]):
-        self.x_data = x_data
-        self.y_data = y_data
-        self.model = None
+class Dataset:
+    """
+    Encapsulation of data needed for linear regression.
+    """
 
-    def __lt__(self):
-        pass
+    x_data: list[float]
+    y_data: list[float]
 
-    def __eq__(self):
-        pass
+    @staticmethod
+    def _uniform_split(data: list[float], n_segments: int):
+        """
+        Generate uniform value-based bounds that define how
+        to split a list of floats into.
+        """
+        return (
+            [-np.inf]
+            + [
+                min(data) + ((max(data) - min(data)) / (i + 1))
+                for i in reversed(range(1, n_segments))
+            ]
+            + [np.inf]
+        )
 
-
-class PiecewiseLinearRegression:
-    # TODO: Need to force that x_data (and y_data conditionally
-    # on x_data) is sorted ascending in order to segment properly.
-    def __init__(self, x_data: list[float], y_data: list[float], n_segments: int):
-        self.x_data = x_data
-        self.y_data = y_data
-        self.n_segments = n_segments
-
-        self.fitted_segments: list[LinearRegression] = []
-
-    def _conditional_sort(self, list1, list2):
+    def _conditional_sort(self, list1: list[float], list2: list[Any]):
         """
         Sort list2 conditional on the sorting of list1.
         """
         return zip(*sorted(zip(list1, list2)))
 
-    def _segment_data(self, data: list[float], n_segments: int) -> list[tuple[int]]:
-        """
-        Splits the provided dataset into a defined
-        number of segments, with an equal number
-        of observations in each segment.
-        """
-        return list(batched(self.data, n_segments))
+    def create_segments(self, n_segments: int) -> list[SegmentedDataset]:
+        # TODO: This is ugly - refactor
+        segment_collection = []
 
-    def _segment_xy(self):
-        """
-        Sorts Y conditional on X, and then splits the data into a number of segments.
-        """
+        segment_bounds = self._uniform_split(self.x_data, n_segments)
         x_data_sorted, y_data_sorted = self._conditional_sort(self.x_data, self.y_data)
-        self._x_data_segmented = self._segment_data(x_data_sorted, self.n_segments)
-        self._y_data_segmented = self._segment_data(y_data_sorted, self.n_segments)
+        segment_x_data = []
+        segment_y_data = []
+        j = 1
+
+        for i in range(len(x_data_sorted)):
+            if x_data_sorted[i] <= segment_bounds[j]:
+                segment_x_data.append(x_data_sorted[i])
+                segment_y_data.append(y_data_sorted[i])
+            else:
+                segment = SegmentedDataset(
+                    x_data=segment_x_data,
+                    y_data=segment_y_data,
+                    lower_bound=segment_bounds[j - 1],
+                    upper_bound=segment_bounds[j],
+                )
+                segment_collection.append(segment)
+                segment_x_data = [x_data_sorted[i]]
+                segment_y_data = [y_data_sorted[i]]
+
+                j += 1
+        segment = SegmentedDataset(
+            x_data=segment_x_data,
+            y_data=segment_y_data,
+            lower_bound=segment_bounds[j - 1],
+            upper_bound=segment_bounds[j],
+        )
+        segment_collection.append(segment)
+
+        return segment_collection
+
+
+@dataclass
+class SegmentedDataset(Dataset):
+    """
+    Encapsulation of data needed for piecewise linear regression.
+    """
+
+    lower_bound: float | None = None
+    upper_bound: float | None = None
+
+
+class PiecewiseLinearRegression:
+    def __init__(self, data: list[SegmentedDataset]):
+        self.data: list[SegmentedDataset] = data
+        self.fitted_segments: list[LinearRegression] = []
 
     def fit(self) -> None:
         """
@@ -74,7 +110,25 @@ class PiecewiseLinearRegression:
             lr.fit()
             self.fitted_segments.append(lr)
 
-    def predict(self, x_observed: List):
+    def _predict_single(self, x_observed: float):
+        """
+        Iterate through bactched x data and find which
+        segment this value belongs within.
+        """
+        if x_observed < min(self.x_data):
+            pass
+
+    def predict(self, x_observed: list[float]):
         alpha = self.coeff[0]
         beta = self.coeff[1]
         return [alpha + (i * beta) for i in x_observed]
+
+
+if __name__ == "__main__":
+    pwlr_data = Dataset(
+        x_data=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        y_data=[2, 4, 6, 8, 10, 12, 14, 16, 18],
+    )
+    segmented_data = pwlr_data.create_segments(n_segments=3)
+    print(segmented_data)
+    pwlr = PiecewiseLinearRegression(segmented_data)
